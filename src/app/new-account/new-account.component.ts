f6c3bad36718d0cb406c1d8ebdb4fb240c6e7233
Itamar Atid <itamar@softwave.co.il>
2022-05-15 02:19:11 +0300

    removed aacountservice from providers array in bot account and newAccount components for being able to use same instance in all of them - the appComponent instance of account service

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -6,7 +6,7 @@ import { LoggingService } from '../shared/logging.service';
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService, AccountService]
+  providers: [LoggingService]
 })
 export class NewAccountComponent  {
  
@@ -15,7 +15,6 @@ export class NewAccountComponent  {
 
   onCreatAccount(accountName: string, accountStatus:string){
     this.accountService.addAccount(accountName,accountStatus);
- 
     this.loggingService.logStatusChange(accountStatus);
   }
   
